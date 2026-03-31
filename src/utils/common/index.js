fdc5b82da1e1e106c2308ed0a1640013b6865b18
Tejas Ganesh <tejasganesh323@gmail.com>
2026-03-31 20:11:42 +0530

    created seat model and migration, and seeded seat table

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     ErrorResponse : require('./errorresponse'),
-    SuccessResponse : require('./success-response')
+    SuccessResponse : require('./success-response'),
+    ENUMS : require('./enums')
 }
